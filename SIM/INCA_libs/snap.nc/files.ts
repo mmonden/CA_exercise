7303d740e8c8e4f8ba333c064a8caf001cb48933
Matthias <48888533+mmonden@users.noreply.github.com>
2023-04-20 10:47:51 +0200

    Add files via upload

---
+++ SIM/INCA_libs/snap.nc/files.ts
@@ -0,0 +1,14 @@
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/alu.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/alu_control.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/branch_unit.v
+1681979816 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/control_unit.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/cpu.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/immediate_extend_unit.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/mux_2.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/pc.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/reg_arstn.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/reg_arstn_en.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/register_file.v
+1680448715 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/RTL/sram.v
+1680432782 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/cpu_tb.v
+1680442097 /users/students/r0793013/Downloads/CA_processor_design_2023/CA_Exercises/Verilog/sky130_sram_2rw.v
